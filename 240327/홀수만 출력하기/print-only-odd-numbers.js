94c483984129ffc45ea27b6f1ef7b7211859a1b1
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-27 23:11:51 +0900

    Title: 홀수만 출력하기, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/홀수만 출력하기/print-only-odd-numbers.js
+++ 240327/홀수만 출력하기/print-only-odd-numbers.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 let result = '';
-const input = fs.readFileSync(0).toString().trim().split("\n").map(el => {
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+input.shift();
+input.map(el => {
     if(el % 2 === 1 && el % 3 === 0) {
         return result += el + '\n';
     }
 });
-
 console.log(result);
