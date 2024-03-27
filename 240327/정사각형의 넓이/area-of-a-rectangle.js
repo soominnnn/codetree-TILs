85fea9b9e46de981fbbd247c85713f03eb410b2d
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-27 22:01:21 +0900

    Title: 정사각형의 넓이, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240327/정사각형의 넓이/area-of-a-rectangle.js
+++ 240327/정사각형의 넓이/area-of-a-rectangle.js
@@ -3,4 +3,4 @@ const input = fs.readFileSync(0).toString().trim();
 const n = Number(input);
 const area = n * n;
 
-console.log(area < 5 ? `${area}\ntiny` : area);
+console.log(area >= 5 ? area : `${area}\ntiny`);
