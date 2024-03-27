aa0338cf87f32825ef60e267c63befcfe88d3392
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-27 15:02:34 +0900

    Title: 실수 입력받아 계산, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 240327/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split("\n");
 
-let a = input[0];
-let b = input[1];
+let a = Number(input[0]);
+let b = Number(input[1]);
+let sum = a + b
 
-console.log((a + b).toFixed(2));
+console.log(sum.toFixed(2));
