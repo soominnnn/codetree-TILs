8c125ba0107debafdc8ace454eb436975761ea9c
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-27 22:48:11 +0900

    Title: 두 사람, Time: 201ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240327/두 사람/two-person.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+for(let i = 0; i < input.length; i++) {
+    const information = input[i].split(" ");
+    const age = +information[0];
+    const sex = information[1];
+    
+    if(age >= 19 && sex === 'M') {
+        return console.log(1);
+    }
+    return console.log(0);
+}
