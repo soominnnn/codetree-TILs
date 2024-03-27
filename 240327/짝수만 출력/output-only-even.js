f33920644303ee9a5ff0848fbbc69e0fc0bd77b0
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-27 22:58:15 +0900

    Title: 짝수만 출력, Time: 207ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240327/짝수만 출력/output-only-even.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+let result = '';
+
+for(let i = +input[0]; i <= +input[1]; i++) {
+    result += i % 2 === 0 ? i + ' ' : '';
+}
+
+console.log(result);
