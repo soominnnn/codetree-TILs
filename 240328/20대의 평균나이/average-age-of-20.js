62802659dcc44b64a6711ded883eafbc2d2bbdc2
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-28 20:16:53 +0900

    Title: 20대의 평균나이, Time: 213ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240328/20대의 평균나이/average-age-of-20.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+let sum = 0
+let result = 0;
+
+for(let i = 0; i < input.length; i++) {
+    if(+input[i] >= 30) {
+        result = (sum / (i)).toFixed(2);
+        console.log(result);
+        break;
+    }
+    
+    sum += +input[i];
+}
