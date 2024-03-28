2e297dc46014b633ac3e25fbbce98bf0e19e9502
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-28 23:01:04 +0900

    Title: 1이 되는 순간까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240328/1이 되는 순간까지/until-the-moment-I-reach-one.js
+++ 240328/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -3,13 +3,14 @@ const input = fs.readFileSync(0).toString().trim();
 let count = 0;
 
 function findOne(n) {
+    let remainder = n % 2 === 0 ? Math.floor(n / 2) : Math.floor(n / 3);
     count += 1;
     
-    let remainder = n % 2 === 0 ? Math.floor(n / 2) : Math.floor(n / 3);
-
     if(remainder === 1) {
         return count;
     }
+
+
     return findOne(remainder);
 }
 
