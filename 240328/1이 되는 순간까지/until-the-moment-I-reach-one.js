5f2255bae5ee4f4023dc561aca6865e581df6c24
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-28 23:02:52 +0900

    Title: 1이 되는 순간까지, Time: 196ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240328/1이 되는 순간까지/until-the-moment-I-reach-one.js
+++ 240328/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -5,8 +5,8 @@ let count = 0;
 function findOne(n) {
     let remainder = n % 2 === 0 ? Math.floor(n / 2) : Math.floor(n / 3);
     count += 1;
-    
-    if(remainder === 1) {
+
+    if(remainder <= 1) {
         return count;
     }
 
