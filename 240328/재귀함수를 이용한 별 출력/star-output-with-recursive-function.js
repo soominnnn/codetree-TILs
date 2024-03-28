bd18def146abd2a5c90b73555f8e0856c21592e7
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-28 22:10:18 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 210ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240328/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = +fs.readFileSync(0).toString().trim();
+let result = '';
+
+function printStar(n) {
+    if(n === 0) return;
+
+    printStar(n-1);
+    result += '*'.repeat(n) + '\n';
+}
+
+printStar(input);
+console.log(result);
