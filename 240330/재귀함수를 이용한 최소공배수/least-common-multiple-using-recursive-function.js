b665b3c558d8dc9651e5b97feb2f03cb29be585f
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-30 02:57:41 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240330/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync().toString().trim().split("\n");
+const n = input.shift();
+const array = input[0].split(" ");
+
+function gcd(a, b) {
+    if(b === 0){
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+function findLcm(arr, n) {
+    if(n === 1) {
+        return arr[0];
+    }
+    
+    return lcm(arr[n - 1], findLcm(arr, n - 1));
+}
+
+console.log(findLcm(array, n));
