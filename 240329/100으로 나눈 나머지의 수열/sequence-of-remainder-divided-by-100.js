170447e510b42bd2352cc4302cb3902d24fa0ee2
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-29 23:39:58 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 203ms, Memory: 15MB, Status: Passed - CodeTree

--- 240329/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
+++ 240329/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
@@ -2,11 +2,15 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
 function sequence(n) {
-    if(n === 2) {
+    if(n === 1) {
         return 2;
     }
 
-    return sequence(n - 1) ** 2;
+    if(n === 2) {
+        return 4;
+    }
+
+    return (sequence(n - 1) * sequence(n - 2)) % 100;
 }
 
-console.log(sequence(+input) % 100);
+console.log(sequence(+input));
