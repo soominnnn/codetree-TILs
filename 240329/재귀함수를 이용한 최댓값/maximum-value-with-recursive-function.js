6f32c74c0c155d88f9a1e2ad6c4c071e27545f53
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-29 21:32:12 +0900

    Title: 재귀함수를 이용한 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240329/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 240329/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -6,7 +6,9 @@ let maxNumber = 0;
 
 function findMaxNumber(n) {
     if(n === 0) {
-        return array[0];
+        if(maxNumber <= +array[0]) {
+            return maxNumber = +array[0];
+        }
     }
 
     if(maxNumber <= +array[n]) {
