ffc3f273269596cc82bdf335fd2f9ced0fef60de
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-04 18:48:35 +0900

    Title: 윤년인가, Time: 189ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240404/윤년인가/is-leap-year.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+if(+input % 4 === 0) {
+    if(+input % 100 === 0 && +input % 400 === 0) {
+        return console.log("false");
+    }
+    return console.log("true");
+}
+else {
+    return console.log("false");
+}
