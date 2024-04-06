7c81234a03eebf13c0749608dcb5246b2b55a2fe
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-06 17:56:07 +0900

    Title: 비교 연산, Time: 238ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240406/비교 연산/comparison-operator.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const [a, b] = [+input[0], +input[1]];
+let result = '';
+
+result += a >= b ? "1\n" : "0\n";
+result += a > b ? "1\n" : "0\n";
+result += a <= b ? "1\n" : "0\n";
+result += a < b ? "1\n" : "0\n";
+result += a === b ? "1\n" : "0\n";
+result += a !== b ? "1\n" : "0\n";
+
+console.log(result);
