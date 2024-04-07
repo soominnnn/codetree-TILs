acc90d0cfdf7ccf7ec9df665142435da1a13144e
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-07 17:15:56 +0900

    Title: 숫자의 조건 여부 3, Time: 236ms, Memory: 15MB, Status: Passed - CodeTree

--- 240407/숫자의 조건 여부 3/number's-condition-3.js
+++ 240407/숫자의 조건 여부 3/number's-condition-3.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
-console.log(+input % 13 || +input % 19 ? "True" : "False");
+console.log(+input % 13 === 0 || +input % 19 === 0 ? "True" : "False");
