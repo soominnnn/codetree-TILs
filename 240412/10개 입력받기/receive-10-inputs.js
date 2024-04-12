34d7658cbfd4c6c98bac11af34b0bdfa18c0f27b
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-12 13:01:00 +0900

    Title: 10개 입력받기, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240412/10개 입력받기/receive-10-inputs.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ');
+const index = input.indexOf('0');
+const array = input.filter((_, idx) => idx < index);
+const sum = array.reduce((a, b) => Number(a) + Number(b), 0);
+const average = (sum / array.length).toFixed(1);
+
+console.log(sum, average);
