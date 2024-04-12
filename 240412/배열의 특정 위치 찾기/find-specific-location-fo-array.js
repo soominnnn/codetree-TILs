fccd1a7b5aa58dc41382aadbb299e18fdb276685
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-12 16:41:40 +0900

    Title: 배열의 특정 위치 찾기, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240412/배열의 특정 위치 찾기/find-specific-location-fo-array.js
+++ 240412/배열의 특정 위치 찾기/find-specific-location-fo-array.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split(" ");
-const even = input.filter(el => el % 2 === 0);
+const even = input.filter((_, idx) => idx % 2 !== 0);
 const evenSum = even.reduce((a, b) => Number(a) + Number(b), 0);
-const multiplesOfThree = input.filter(el => el % 3 === 0);
+const multiplesOfThree = input.filter((_, idx) => idx % 3 === 2);
 const multiplesSum = multiplesOfThree.reduce((a, b) => Number(a) + Number(b), 0);
-const average = Math.floor(multiplesSum / multiplesOfThree.length).toFixed(1);
+const average = (multiplesSum / multiplesOfThree.length).toFixed(1);
 
 console.log(evenSum, average);
