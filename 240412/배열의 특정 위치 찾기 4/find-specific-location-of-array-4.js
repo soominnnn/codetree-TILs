2cea46135cf727a21bba612015191da8396c4e8c
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-12 16:30:33 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

--- 240412/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.js
+++ 240412/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split(' ');
 const index = input.indexOf('0') !== -1 ? input.indexOf('0') : input.length;
 const array = input.filter((el, idx) => el % 2 === 0 && idx < index);
-const sum = array.reduce((a, b) => Number(a) + Number(b));
+const sum = array.reduce((a, b) => Number(a) + Number(b), 0);
 
 console.log(array.length, sum);
