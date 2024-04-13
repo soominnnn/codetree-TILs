c2d7e325831d3820b7ea0c90a31780474449c7b8
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-13 19:39:49 +0900

    Title: 일 수 구하기, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240413/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+switch(+input) {
+    case 1: console.log('31'); break;
+    case 2: console.log('28'); break;
+    case 3: console.log('31'); break;
+    case 4: console.log('30'); break;
+    case 5: console.log('31'); break;
+    case 6: console.log('30'); break;
+    case 7: console.log('31'); break;
+    case 8: console.log('31'); break;
+    case 9: console.log('30'); break;
+    case 10: console.log('31'); break;
+    case 11: console.log('30'); break;
+    case 12: console.log('31'); break;
+}
