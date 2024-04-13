163263c7074ddb5081355b354bfd1af762da4cd5
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-13 19:48:56 +0900

    Title: 배수의 개수 2, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

--- 240413/배수의 개수 2/number-of-multipliers-2.js
+++ 240413/배수의 개수 2/number-of-multipliers-2.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const result = input.filter(el => +el % 2 === 0).length;
+const result = input.filter(el => +el % 2 === 1).length;
 
 console.log(result);
