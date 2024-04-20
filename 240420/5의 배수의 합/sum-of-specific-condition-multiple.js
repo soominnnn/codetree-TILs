38362ca927d3aa9433d58468a99655365fbaf13d
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-20 22:28:15 +0900

    Title: 5의 배수의 합, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240420/5의 배수의 합/sum-of-specific-condition-multiple.js
+++ 240420/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -3,8 +3,14 @@ const input = fs.readFileSync(0).toString().trim().split(' ');
 const [a, b] = [+input[0], +input[1]];
 let result = 0;
 
-for(let i = a; i <= b; i++) {
-    result += i % 5 === 0 ? i : 0;
+if(a > b) {
+    for(let i = a; i <= b; i++) {
+        result += i % 5 === 0 ? i : 0;
+    }
+} else {
+    for(let i = b; i <= a; i++) {
+        result += i % 5 === 0 ? i : 0;
+    }
 }
 
 console.log(result);
