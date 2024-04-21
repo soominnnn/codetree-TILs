264bf135070f80206768fa96de59df9ec18c0ba8
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-04-21 15:16:47 +0900

    Title: 1부터 곱셈하기, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240421/1부터 곱셈하기/multiple-from-one.js
+++ 240421/1부터 곱셈하기/multiple-from-one.js
@@ -4,8 +4,9 @@ let result = 1;
 
 for(let i = 1; i <= 10; i++) {
     if(result >= +input) {
-        console.log(i - 1);
-        break;
+        return console.log(i - 1);
     }
     result *= i;
 }
+
+console.log("10");
