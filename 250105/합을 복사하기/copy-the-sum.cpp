fcd9a5b0a2171046f86edd1bad8b9128c75f9ad4
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-05 01:53:31 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250105/합을 복사하기/copy-the-sum.cpp
+++ 250105/합을 복사하기/copy-the-sum.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main() {
     int a = 1, b = 2, c = 3;
-    int result = a + b + c;
-    a = b = c = result;
+    a = b = c = a + b + c;
 
     cout << a << " " << b << " " << c;
     return 0;
