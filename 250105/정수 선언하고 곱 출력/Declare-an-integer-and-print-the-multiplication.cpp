598fe58e0bd6c62dee56be9f2a8b095028c2b126
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-05 01:23:00 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 26;
+    int num2 = 5;
+    int answer = num1 * num2;
+
+    cout << num1 << " * " << num2 << " = " << answer;
+    return 0;
+}
