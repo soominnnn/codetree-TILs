b24308e8b2dca605a38df4ed360df88d0bbef60c
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-05 20:20:23 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a_math, a_eng;
+    int b_math, b_eng;
+
+    cin >> a_math >> a_eng >> b_math >> b_eng;
+
+    if (a_math > b_math && a_eng > b_eng) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
