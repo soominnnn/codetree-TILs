cf5090016adf1dcb9aef8ccd7f63ff86e70b6534
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-05 20:22:31 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/차례로 출력/print-in-order.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start = 5, end = 17;
+
+    for (int i = start; i <= end; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
