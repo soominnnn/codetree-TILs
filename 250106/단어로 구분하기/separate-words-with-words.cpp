97f692ce3d85d812487825523247b53e0e6befa8
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-06 18:45:00 +0900

    Title: 단어로 구분하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
