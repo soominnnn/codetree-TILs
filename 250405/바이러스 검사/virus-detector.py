7b8c1b71064d92b3bb8eb364a568930ed112738e
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-04-05 13:25:57 +0900

    Title: 바이러스 검사, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250405/바이러스 검사/virus-detector.py
@@ -0,0 +1,15 @@
+store_cnt = int(input())
+customer_arr = list(map(int, input().split()))
+leader_cnt, member_cnt = map(int, input().split())
+answer = 0
+
+for cnt in customer_arr:
+    answer += 1
+    cnt -= leader_cnt
+
+    if cnt % member_cnt != 0:
+        answer += cnt // member_cnt + 1
+    else:
+        answer += cnt
+
+print(answer)
