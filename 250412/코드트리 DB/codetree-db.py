e7825510e94c61cecfe51bb8f8ed8ce5386b457d
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-04-12 11:39:08 +0900

    Title: 코드트리 DB, Time: 5029ms, Memory: 40MB, Status: Passed - Codetree

--- 250412/코드트리 DB/codetree-db.py
+++ 250412/코드트리 DB/codetree-db.py
@@ -1,53 +1,54 @@
+from bisect import bisect_right, insort
+
+table = {}              # name -> value
+sorted_values = []      # 항상 정렬된 상태 유지
+value_to_name = {}      # value -> name (unique value니까 가능)
+
 def init():
     table.clear()
+    sorted_values.clear()
+    value_to_name.clear()
 
 def insert(input):
     query = input.split()
     name, value = query[1], int(query[2])
 
-    if name in table or value in table.values():
+    if name in table or value in value_to_name:
         print(0)
     else:
         table[name] = value
+        value_to_name[value] = name
+        insort(sorted_values, value)  # O(log N)
         print(1)
+
 def delete(input):
     name = input.split()[1]
 
     if name in table:
-        print(table.pop(name))
+        value = table.pop(name)
+        sorted_values.remove(value)  # O(N)이지만 value가 unique니까 괜찮음
+        value_to_name.pop(value)
+        print(value)
     else:
         print(0)
 
 def rank(input):
     k = int(input.split()[1])
-    sorted_table = sorted(table.values())
 
-    if k > len(sorted_table):
-        return print("None")
-
-    for key, value in table.items():
-        if sorted_table[k-1] == value:
-            return print(key)
+    if k > len(sorted_values):
+        print("None")
+    else:
+        value = sorted_values[k - 1]
+        print(value_to_name[value])
 
 def sum_value(input):
-    s = 0
     k = int(input.split()[1])
-
-    for key, value in table.items():
-        if value <= k:
-            s += value
-
-    if s == 0:
-        print(0)
-    else:
-        print(s)
+    idx = bisect_right(sorted_values, k)  # k 이하 값의 개수
+    print(sum(sorted_values[:idx]))
 
 Q = int(input())
-table = {}
-
-for idx in range(Q):
+for _ in range(Q):
     inp = input()
-
     if inp.startswith("init"):
         init()
     elif inp.startswith("insert"):
