8c0f8c6a481e50a82d8af106384476d4481eb806
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-09 23:44:26 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251009/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n = 0;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << n;
+    return 0;
+}
