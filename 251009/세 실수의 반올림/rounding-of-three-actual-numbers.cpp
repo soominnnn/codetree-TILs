a75af8a23dfebc23ab5333a9ec320e428cfb6825
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-09 23:46:10 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251009/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << "\n" << b << "\n" << c << "\n";
+    return 0;
+}
