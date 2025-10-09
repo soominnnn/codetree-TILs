577cf6b90d954b2981981e63b64fd2d88ca7e576
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-09 23:35:43 +0900

    Title: 사칙연산 연습, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251009/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+
+    int result1 = c / 8;
+    int result2 = c % 8;
+
+    cout << result1 << endl << result2;
+    return 0;
+}
