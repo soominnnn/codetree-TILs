e42f7fdf804e6911390badfdf2e8ea98c4e69cf6
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-09 23:40:58 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251009/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int point = 0;
+
+    cin >> point;
+    cout << "Your score is " << point << " point.";
+    return 0;
+}
